6351ee145078f6cc63e8a058698e74d1a06b4455
unknown <j.poliveira90@outlook.com>
2023-02-06 09:24:55 -0500

    Uploaded axios into project + connected both the backend and this frontend project + was able to show music library table data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+async function getAllSongs(){
+  let response = await axios.get('http://127.0.0.1:8000/api/music/');
+  setSongs(response.data);
+
+}
+
   return (
-    <div >
-      <h3>Music Library</h3>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs.map((song) => {
+            return (
+              <tr>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.release_date}</td>
+                <td>{song.genre}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
